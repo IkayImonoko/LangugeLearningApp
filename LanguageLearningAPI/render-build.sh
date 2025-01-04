4a5c1587c78a36d4a87b931313ae37bb2c9b315b
Ikay Imonoko <ntolok1990@gmail.com>
2025-01-04 17:42:29 +0100

    Commit 4

--- LanguageLearningAPI/render-build.sh
+++ LanguageLearningAPI/render-build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Установка .NET SDK
-curl -sSL https://dotnet.microsoft.com/download/dotnet/8.0/installer/linux-x64 | bash
+curl -sSL https://dotnet.microsoft.com/download/dotnet/8.0/installer/linux-x64 -o dotnet-sdk.tar.gz
+tar -xzf dotnet-sdk.tar.gz
+
 
 # Добавление .NET в PATH
 echo 'export PATH=$PATH:$HOME/.dotnet' >> ~/.bashrc
